16d2a0a40c5115d1c8b263a6be583d2615733b4e
khoagrai <khoagrai@gmail.com>
2025-11-14 19:54:08 +0700

    Update CuaHang.sql

--- CuaHang.sql
+++ CuaHang.sql
@@ -1,6 +1,10 @@
+-- ==============================
+-- BẢNG CỬA HÀNG
+-- ==============================
 CREATE TABLE CuaHang (
-    MaCH VARCHAR(20) NOT NULL PRIMARY KEY,
+    MaCH VARCHAR(8) PRIMARY KEY,
     TinhThanh NVARCHAR(50),
     PhuongXa NVARCHAR(50),
     DuongVaSoNha NVARCHAR(100)
 );
+GO
